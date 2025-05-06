c25ac827f1b174f1ebb8a992f9b9f4aefbf94558
Do Anh <doanh20042018@gmail.com>
2025-05-06 16:36:48 +0700

    complete bai1

--- Bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.bai1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB, edtResult;
+    Button btnSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtResult = findViewById(R.id.edtResult);
+        btnSum = findViewById(R.id.btnSum);
+
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int a = Integer.parseInt(edtA.getText().toString());
+                    int b = Integer.parseInt(edtB.getText().toString());
+                    int sum = a + b;
+                    edtResult.setText(String.valueOf(sum));
+                } catch (NumberFormatException e) {
+                    edtResult.setText("Lỗi nhập liệu");
+                }
+            }
         });
     }
 }
